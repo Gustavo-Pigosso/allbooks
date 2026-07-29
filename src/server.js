f4becf297996529edb8c21776f2eef48e0b5b3a7
Gustavo Pigosso <gustavopigosso250@gmail.com.br>
2026-07-29 17:19:32 -0300

    Altera rota

--- src/server.js
+++ src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas vindas a API do AllBooks")
-  console.log("API disponível atraves da url http://localhost:8000")
-=======
   console.log("Acesse essa API em http://localhost:8000")
->>>>>>> 08830bea878e3242828e56ab6ea3c2272722bcb4
 })
